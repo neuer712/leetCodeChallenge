9048869a9860af705fd3635685e7f217f2cd153f
Wei.Lai(Future) <neuer712@163.com>
2020-10-11 23:17:39 +0800

    add the solution of problem11

---
+++ leetCodeChallenge/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace leetCodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Problem 11: Container With Most Water: " + new _11_ContainerWithMostWater().MaxArea(new int[]{1,8,6,2,5,4,8,3,7}));
+        }
+    }
+}
---
+++ leetCodeChallenge/problems/_11_ContainerWithMostWater.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+
+namespace leetCodeChallenge
+{
+    /// <summary>
+    /// 11. Container With Most Water
+    /// </summary>
+    public class _11_ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            if (height == null || height.Length < 2)
+            {
+                return 0;
+            }
+            int i = 0, j = height.Length - 1, maxValue = 0;
+            int leftBigWall = 0, rightBigWall = 0;
+            while (i < j)
+            {
+                int w = j - i;
+                if (height[i] > height[j])
+                {
+                    if (height[j] > rightBigWall)
+                    {
+                        rightBigWall = height[j];
+                        int currentValue = height[j] * w;
+                        if (currentValue > maxValue)
+                        {
+                            maxValue = currentValue;
+                        }
+                    }
+                    j--;
+                }
+                else
+                {
+                    if (height[i] > leftBigWall)
+                    {
+                        leftBigWall = height[i];
+                        int currentValue = height[i] * w;
+                        if (currentValue > maxValue)
+                        {
+                            maxValue = currentValue;
+                        }
+                    }
+                    i++;
+                }
+            }
+            return maxValue;
+        }
+    }
+
+}
