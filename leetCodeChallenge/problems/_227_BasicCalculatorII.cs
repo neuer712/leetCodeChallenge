de65f5dcde74a25da96e6056a39e7804dee2c6fe
Lai Wei (Future) <neuer712@163.com>
2023-02-18 22:26:13 +0100

    finished challenge 227 without using stack

---
+++ leetCodeChallenge/problems/_227_BasicCalculatorII.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCodeChallenge.problems
+{
+    public class _227_BasicCalculatorII_noneStack  //TODO make a stack version!
+    {
+        public int Calculate(string s)
+        {
+            s = s + "+0";
+            int lastAdd = 0;
+            char lastOperationDown = '+';
+            char lastOperationUp = '*';
+            int lastMulti = 1;
+            bool up = false;
+            int lastNumStart = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                char current = s[i];
+                
+                switch (current)
+                {
+                    case '+':
+                        {
+                            int lastNumber = Convert.ToInt32(s.Substring(lastNumStart, i - lastNumStart));
+                            if (up)
+                            {
+                                lastAdd = eval(lastAdd, eval(lastMulti, lastNumber, lastOperationUp), lastOperationDown);
+                                up = false;
+
+                            }
+                            else
+                            {
+                                lastAdd = eval(lastAdd, lastNumber, lastOperationDown);
+                            }
+                            lastOperationDown = '+';
+                            lastNumStart = i + 1;
+                            break;
+                        }
+                    case '-':
+                        {
+                            int lastNumber = Convert.ToInt32(s.Substring(lastNumStart, i - lastNumStart));
+                            if (up)
+                            {
+                                lastAdd = eval(lastAdd, eval(lastMulti, lastNumber, lastOperationUp), lastOperationDown);
+                                up = false;
+
+                            }
+                            else
+                            {
+                                lastAdd = eval(lastAdd, lastNumber, lastOperationDown);
+                            }
+                            lastOperationDown = '-';
+                            lastNumStart = i + 1;
+                            break;
+                        }
+                    case '*':
+                        {
+                            int lastNumber = Convert.ToInt32(s.Substring(lastNumStart, i - lastNumStart));
+                            if (up)
+                            {
+                                lastMulti = eval(lastMulti, lastNumber, lastOperationUp);
+                            }
+                            else
+                            {
+                                up = true;
+                                lastMulti = lastNumber;
+                            }
+                            lastOperationUp = '*';
+                            lastNumStart = i + 1;
+                            break;
+                        }
+                    case '/':
+                        {
+                            int lastNumber = Convert.ToInt32(s.Substring(lastNumStart, i - lastNumStart));
+                            if (up)
+                            {
+                                lastMulti = eval(lastMulti, lastNumber, lastOperationUp);
+                            }
+                            else
+                            {
+                                up = true;
+                                lastMulti = lastNumber;
+                            }
+                            lastOperationUp = '/';
+                            lastNumStart = i + 1;
+                            break;
+                        }
+                }
+            }
+            return lastAdd;
+
+        }
+
+        private int eval(int left, int right, char operation)
+        {
+            switch (operation)
+            {
+                case '+': return left + right;
+                case '-': return left - right;
+                case '*': return left * right;
+                default: return left / right;
+            }
+        }
+    }
+}
