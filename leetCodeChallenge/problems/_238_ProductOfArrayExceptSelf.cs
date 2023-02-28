f2d70e7118340c44e7a179426ec44282e8f56c38
Lai Wei <laiwe@microsoft.com>
2023-02-28 13:14:41 +0100

    Create _238_ProductOfArrayExceptSelf.cs

---
+++ leetCodeChallenge/problems/_238_ProductOfArrayExceptSelf.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCodeChallenge.problems
+{
+    public class _238_ProductOfArrayExceptSelf
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            bool hasZero = false;
+            int current = 1;
+            int[] left = new int[nums.Length];
+            left[0] = 1;
+            int[] right = new int[nums.Length];
+            right[nums.Length - 1] = 1;
+            int i = 0;
+            for (; i < nums.Length - 1; i++)
+            {
+                if (nums[i] == 0) break;
+                current *= nums[i];
+                left[i + 1] = current;
+            }
+
+            while (i < nums.Length - 1)
+            {
+                left[i + 1] = 0;
+                i++;
+            }
+
+            current = 1;
+            for (; i > 0; i--)
+            {
+                if (nums[i] == 0) break;
+                current *= nums[i];
+                right[i - 1] = current;
+            }
+
+            while (i > 0)
+            {
+                right[i - 1] = 0;
+                i--;
+            }
+
+            int[] results = new int[nums.Length];
+            for (; i < nums.Length; i++)
+            {
+                results[i] = left[i] * right[i];
+            }
+
+            return results;
+
+        }
+    }
+}
