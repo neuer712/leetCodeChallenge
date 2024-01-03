957f331838cb4cea8b07d3a55fe8977254a5b900
Lai Wei (Future) <neuer712@163.com>
2024-01-03 21:46:39 +0100

    new accepts

--- leetCodeChallenge/problems/_1143_LongestCommonSubsequence.cs
+++ leetCodeChallenge/problems/_1143_LongestCommonSubsequence.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace leetCodeChallenge.problems
 {
-    internal class Class1
+    public class _1143_LongestCommonSubsequence
     {
+        public int LongestCommonSubsequence(string text1, string text2)
+        {
+            int[][] matrix = new int[text1.Length][];
+            for (int i = 0; i < text1.Length; i++)
+            {
+                matrix[i] = new int[text2.Length];
+                for (int j = 0; j < text2.Length; j++)
+                {
+                    if (text1[i] == text2[j])
+                    {
+                        matrix[i][j] = -1;
+                    }
+                    else
+                    {
+                        matrix[i][j] = 0;
+                    }
+                }
+            }
+
+            if (matrix[0][0] == -1)
+            {
+                matrix[0][0] = 1;
+            }
+
+            for(int j = 1; j < text2.Length; j++)
+            {
+                if (matrix[0][j - 1] == 1 || matrix[0][j] != 0)
+                {
+                    matrix[0][j] = 1;
+                }
+            }
+
+            for (int i = 1; i < text1.Length; i++)
+            {
+                if (matrix[i - 1][0] != 0 || matrix[i][0] != 0)
+                {
+                    matrix[i][0] = 1;
+                }
+                for (int j = 1; j < text2.Length; j++)
+                { 
+                    int localmax = Math.Max(matrix[i - 1][j], matrix[i][j - 1]);
+                    if (matrix[i][j] == -1)
+                    { 
+                        localmax = Math.Max(localmax, matrix[i-1][j-1] + 1);
+                    }
+
+                    matrix[i][j] = localmax;
+                }
+            }
+
+            return matrix[text1.Length - 1][text2.Length - 1];
+        }
     }
 }
---
+++ leetCodeChallenge/problems/_39_CombinationSum.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCodeChallenge.problems
+{
+    public class _39_CombinationSum
+    {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            IList<IList<int>> results = new List<IList<int>>();
+            Array.Sort<int>(candidates);
+            tryTheCombination(candidates, target, 0, new List<int>(), 0, results);
+            return results;
+        }
+
+        public void tryTheCombination(int[] candidates, int target, int currentPos,List<int> currentList, int currentSum, IList<IList<int>> results)
+        {
+            for (int i = currentPos; i < candidates.Length; i++)
+            {
+                int testSum = currentSum + candidates[i];
+                if (testSum > target)
+                {
+                    return;
+                }
+
+                if (testSum == target)
+                {
+                    currentList.Add(candidates[i]);
+                    results.Add(new List<int>(currentList));
+                }
+
+                else
+                {
+                    currentList.Add(candidates[i]);
+                    tryTheCombination(candidates, target, i, currentList, testSum, results);
+                }
+
+                int currentCount = currentList.Count;
+                if (currentCount > 0)
+                { 
+                    currentList.RemoveAt(currentCount - 1);
+                }
+            }
+        }
+    }
+}
