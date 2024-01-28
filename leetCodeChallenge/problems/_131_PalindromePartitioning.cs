a4a85597ee22dae8403abe32ed1c5878977636a2
Lai Wei (Future) <neuer712@163.com>
2024-01-28 19:59:40 +0100

    new ACs

---
+++ leetCodeChallenge/problems/_131_PalindromePartitioning.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCodeChallenge.problems
+{
+    //need to think about it.
+    public class _131_PalindromePartitioning
+    {
+        /*
+        public IList<IList<string>> Partition(string s)
+        {
+            int length = s.Length;
+            int totalPossibleCenterLength = length * 2 - 1;
+            Dictionary<int,int> maxPalindromeAtPos = new Dictionary<int,int>();
+            for (int i = 0; i < totalPossibleCenterLength; i++)
+            {
+                maxPalindromeAtPos[i] = findPalindromePossibility(s, i);
+            }
+
+
+        }
+
+        public bool PutPart(string s, int pos, Dictionary<int, int> maxPalindromeAtPos, int occupiedLetter, IList<string> partialResult, IList<IList<string>> result)
+        { 
+            for(int i = 0; i < )
+        }
+
+        public int findPalindromePossibility(string s, int pos)
+        {
+            if (pos % 2 == 0)
+            {
+                int actualPos = pos / 2;
+                int i = 0;
+                while (actualPos - (i + 1) > 0 && actualPos + (i + 1) < s.Length && (s[actualPos - (i + 1)] == s[actualPos + (i + 1)]))
+                {
+                    i++;
+                }
+
+                return i;
+            }
+            else
+            {
+                int lowPos = pos / 2, highPos = pos / 2 + 1;
+                int i = 0;
+                while (lowPos > 0 && highPos < s.Length && s[lowPos] == s[highPos]) 
+                {
+                    i++;
+                    lowPos--;
+                    highPos++;
+                }
+
+                return i;
+            }
+        }
+        */
+    }
+}
---
+++ leetCodeChallenge/problems/_24_SwapNodesInPairs.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCodeChallenge.problems
+{
+    public class _24_SwapNodesInPairs
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            ListNode before = new ListNode(val: 0, head);
+            ListNode result = (head!=null && head.next!=null ? head.next : head);
+            while(before != null && before.next!= null && before.next.next !=null) 
+            {
+                var pos1 = before.next;
+                var pos2 = before.next.next;
+                var tail = before.next.next.next;
+                before.next = pos2;
+                pos2.next = pos1;
+                pos1.next = tail ;
+                before = pos1;
+            }
+
+            return result;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
